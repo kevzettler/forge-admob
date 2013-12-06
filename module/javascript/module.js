7a098ceb360fdb2aed5aca5b3f5c9cee82e852de
Kev zettler <kevzettler@gmail.com>
2013-12-06 15:50:17 -0800

    update javascript api

--- module/javascript/module.js
+++ module/javascript/module.js
@@ -1,11 +1,10 @@
 // Expose the native API to javascript
 forge.admob = {
-    showAlert: function (text, success, error) {
-        forge.internal.call('admob.showAlert', {text: text}, success, error);
+    renderBanner: function () {
+        forge.internal.call('admob.renderBanner');
+    },
+
+    refresh: function(){
+      forge.internal.call('admob.refresh');
     }
 };
-
-// Register for our native event
-forge.internal.addEventListener("admob.resume", function () {
-	alert("Welcome back!");
-});
