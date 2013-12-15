c00e1a19d91f8eba1e41142196a39976127f2f99
Kev zettler <kevzettler@gmail.com>
2013-12-14 20:55:13 -0800

    forgot to build the module, version bump

--- inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/armv7s/admob_EventListener.d
+++ inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphoneos/ForgeModule.build/Objects-normal/armv7s/admob_EventListener.d
@@ -1,3 +1,9 @@
 dependencies: \
   /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/admob_EventListener.m \
-  /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/admob_EventListener.h
+  /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/admob_EventListener.h \
+  /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/GADBannerView.h \
+  /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/GADAdSize.h \
+  /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/GADBannerViewDelegate.h \
+  /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/GADRequest.h \
+  /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/GADRequestError.h \
+  /Users/kevzettler/checkiday/forge-admob/inspector/ios-inspector/ForgeModule/globals.h
